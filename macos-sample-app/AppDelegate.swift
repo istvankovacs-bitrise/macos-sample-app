69dee2d8d10c0c1a08e24ab534690f387bd15c2f
trapacska <trapacska@gmail.com>
2017-11-15 16:31:35 +0100

    initial commit

---
+++ macos-sample-app/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macos-sample-app
+//
+//  Created by Tamás Papik on 11/15/17.
+//  Copyright © 2017 Tamas Papik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos-sample-app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macos-sample-app
+//
+//  Created by Tamás Papik on 11/15/17.
+//  Copyright © 2017 Tamas Papik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
